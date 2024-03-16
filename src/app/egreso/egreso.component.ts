b95fac6dc038489b95be8f35b578bb5a84ce432f
julianycia.m@gmail.com <julianycia.m@gmail.com>
2024-03-16 01:10:47 -0300

    agregamos egreso

--- src/app/egreso/egreso.component.ts
+++ src/app/egreso/egreso.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CabeceroComponent } from '../cabecero/cabecero.component';
 import { CommonModule } from '@angular/common';
+import { Egreso } from './egreso.model';
+import { EgresoServicio } from './egreso.servicio';
 
 @Component({
   selector: 'app-egreso',
@@ -10,5 +12,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './egreso.component.css'
 })
 export class EgresoComponent {
+  egresos:Egreso[]=[];
+  
+  constructor(private egresoServicio:EgresoServicio){
+  }
+  ngOnInit(){
+    this.egresos = this.egresoServicio.egresos;
+  }
+  
+  eliminarEgreso(egreso: Egreso){
+    this.egresoServicio.eliminar(egreso);
+  }
 
 }
--- src/app/egreso/egreso.servicio.ts
+++ src/app/egreso/egreso.servicio.ts
@@ -4,6 +4,11 @@ export class EgresoServicio{
     egreso:Egreso[]=[
         new Egreso("Renta Dpto", 900),
         new Egreso("Ropa informal", 200)
-    ]
-  egresos: Egreso[];
+    ];
+
+    eliminar(egreso:Egreso){
+      const indice: number = this.egresos.indexOf(egreso);
+      this.egresos.splice(indice,1); 
+  }
+  egresos: Egreso[] = this.egreso;
 }
